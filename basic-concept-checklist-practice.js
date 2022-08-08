6acef5e209efbcbe63c7284d488e0f9ec6066dea
Hasibul Hasan <hasanhasib7367@gmail.com>
2022-08-09 02:38:49 +0600

    last commit for tonight

--- basic-concept-checklist-practice.js
+++ basic-concept-checklist-practice.js
@@ -46,3 +46,108 @@ console.log(modulo);
 
 // question no 4
 
+// less than
+var num01 = 10;
+var num02 = 7;
+console.log(num01 < num02);
+
+// greater than
+var num01 = 10;
+var num02 = 7;
+console.log(num01 > num02);
+
+// equal to
+var num01 = 10;
+var num02 = 7;
+console.log(num01 === num02);
+
+// not equal to
+var num01 = 10;
+var num02 = 7;
+console.log(num01 !== num02);
+
+// less than or equal to
+var num01 = 10;
+var num02 = 7;
+console.log(num01 <= num02);
+
+// greater than or equal to
+var num01 = 10;
+var num02 = 7;
+console.log(num01 >= num02);
+
+// question no 5
+
+// fulfilling all conditions
+var firstNum = 20
+var secondNum = 5;
+if (firstNum > secondNum && firstNum === secondNum) {
+    console.log('its ok');
+} 
+else {
+    console.log("it's not ok");
+}
+
+// fulfilling at least one condition
+var firstNum = 20
+var secondNum = 5;
+if (firstNum > secondNum || firstNum === secondNum) {
+    console.log('its ok');
+} 
+else {
+    console.log("it's not ok");
+}
+
+// question no 7
+
+// example no 1
+var baby = 0;
+while (baby <= 4) {
+    console.log('I will stop here');
+    baby++;
+}
+
+// example no 2
+var atif = 0;
+while (atif < 10) {
+    console.log('He will continue singing');
+    atif++ 
+}
+
+// example no 3
+var oddNumbers = 7;
+while (oddNumbers <= 19) {
+    console.log(oddNumbers);
+    oddNumbers += 2
+}
+
+// question no 8
+let arr = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+console.log(arr);
+console.log(arr.length);
+arr[4] = 25
+console.log(arr);
+arr.unshift(36, 72);
+console.log(arr);
+arr.push(18, 54);
+console.log(arr);
+arr.shift()
+console.log(arr);
+arr.pop()
+console.log(arr);
+
+// question no 9
+
+// data type number
+let arr2 = [7, 8, 9, 10, 11, 13, 15]
+for(let i = 0; i < arr2.length; i++){
+    const arr1 = arr2[i];
+    console.log(arr1);
+}
+
+// data type string
+let items = ['watch', 'bottle', 'torch', 'pen', 'notebook', 'mouse', 'lighter', 'calculator'];
+for(let i=0; i<items.length; i++){
+    const item = items[i]
+    console.log(item);
+}
